51cc7c7a543e304f806cb217689ae979ed7ef33d
kingstar <admin@baks.dev>
2025-01-20 23:47:20 +0300

    BaksDevelopment

--- UseCases/NewAuthenticated/ProductViewedAuthenticatedHandler.php
+++ UseCases/NewAuthenticated/ProductViewedAuthenticatedHandler.php
@@ -20,21 +20,23 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  */
+
 namespace BaksDev\Products\Viewed\UseCases\NewAuthenticated;
 
 use BaksDev\Products\Viewed\Entity\ProductsViewed;
 use BaksDev\Products\Viewed\Repository\DataUpdate\ProductsViewedDataUpdateInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final readonly class ProductViewedAuthenticatedHandler
 {
     public function __construct(
+        #[Target('productsViewedLogger')] private LoggerInterface $logger,
         private EntityManagerInterface $entityManager,
         private ValidatorInterface $validator,
         private ProductsViewedDataUpdateInterface $dataUpdate,
-        private LoggerInterface $logger,
     ) {}
 
     public function addViewedProduct(ProductViewedAuthenticatedDTO $dto): ProductsViewed|bool
