47b0ffeca7c51379b0b765ff95a591177beb5a5e
Dmitrii <ladovod@gmail.com.com>
2026-05-13 20:39:48 +0500

    Правит bindProjectProfile на isProjectProfile для сезонности

--- Repository/ProductsViewed/ProductsViewedRepository.php
+++ Repository/ProductsViewed/ProductsViewedRepository.php
@@ -638,7 +638,7 @@ final class ProductsViewedRepository implements ProductsViewedInterface
                 'product_project',
                 '
                     product_project.product = product.id
-                    '.(true === $dbal->bindProjectProfile()
+                    '.(true === $dbal->isProjectProfile()
                     ? 'AND product_project.profile = :'.$dbal::PROJECT_PROFILE_KEY
                     : 'AND product_project.profile IS NULL'),
             );
